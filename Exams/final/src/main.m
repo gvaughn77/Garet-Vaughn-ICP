6c42c6fc71f972e61cfc9a690dd93afa111f086f
Garet <gvaughn@utexas.edu>
2017-12-15 12:41:32 -0600

    Sorry main.m file is late, I was waiting on the name of Josh's script and had a final this morning.

---
+++ Exams/final/src/main.m
@@ -0,0 +1,16 @@
+main
+
+clear all;close all; clc
+
+
+%Data reduction and visualization script
+
+reduceDataOfCells
+
+%Obtaining error in tumor cell count
+
+part2
+
+%Mathematical model of tumor growth
+
+part3JoshHenderson.m
